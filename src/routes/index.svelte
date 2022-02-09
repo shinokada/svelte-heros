c32a9d7e8e301677000b3b482c55fffdd6aedcbe
Shinichi Okada <okada.shin@gmail.com>
2022-02-09 20:00:31 +0900

    .npmignore added due to DS_Store is included in package.json.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,22 @@
 <script>
-  import { AcademicCapIcon } from "$lib/heroicons/outline";
+  import {
+    AcademicCapIcon,
+    AdjustmentsIcon,
+    AnnotationIcon,
+    ArrowDownIcon,
+  } from "$lib/heroicons/outline";
 </script>
 
 <AcademicCapIcon />
 
 <AcademicCapIcon className="h-5 w-5 text-blue-500" />
 
-<AcademicCapIcon className="h-40 w-40 text-blue-500" />
+<AcademicCapIcon className="h-20 w-20 text-blue-500" />
 
-<AcademicCapIcon className="h-80 w-80 text-red-900" />
+<AcademicCapIcon className="h-20 w-20 text-red-900" />
+
+<AdjustmentsIcon className="h-10 w-10 text-yellow-500" />
+
+<AnnotationIcon className="h-10 w-10 text-green-300" />
+
+<ArrowDownIcon className="h-10 w-10 text-blue-300" />
