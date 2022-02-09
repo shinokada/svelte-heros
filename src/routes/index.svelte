2a7c08de5b9d542c64936158747c184f8761caaa
Shinichi Okada <okada.shin@gmail.com>
2022-02-10 06:25:46 +0900

    Icon-names.md is added.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,16 @@
 <script>
   import {
-    AcademicCapIcon,
-    AdjustmentsIcon,
-    AnnotationIcon,
-    ArrowDownIcon,
-  } from "$lib/heroicons/outline";
+    ChevronLeftIconSolid,
+    CurrencyRupeeIconSolid,
+    MenuAlt1IconOutline,
+    ThumbDownIconOutline,
+  } from "$lib";
 </script>
 
-<AcademicCapIcon />
+<ChevronLeftIconSolid className="h-5 w-5 text-blue-500" />
 
-<AcademicCapIcon className="h-5 w-5 text-blue-500" />
+<CurrencyRupeeIconSolid className="h-20 w-20 text-blue-500" />
 
-<AcademicCapIcon className="h-20 w-20 text-blue-500" />
+<MenuAlt1IconOutline className="h-20 w-20 text-red-900" />
 
-<AcademicCapIcon className="h-20 w-20 text-red-900" />
-
-<AdjustmentsIcon className="h-10 w-10 text-yellow-500" />
-
-<AnnotationIcon className="h-10 w-10 text-green-300" />
-
-<ArrowDownIcon className="h-10 w-10 text-blue-300" />
+<ThumbDownIconOutline className="h-10 w-10 text-yellow-500" />
