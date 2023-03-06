5e59a310f8afa76c407cd148667c584aeffae97f
n4j1Br4ch1D <kawner93@gmail.com>
2023-03-06 11:30:12 +0100

    fix wrong viewBox for solid

--- src/lib/MinusSmall.svelte
+++ src/lib/MinusSmall.svelte
@@ -2,22 +2,18 @@
   export let size = '24';
   export let color = 'currentColor';
   export let variation: 'solid' | 'outline' = 'outline';
-  let viewBox: string;
+  let viewBox: string = '0 0 24 24';
   let svgpath: string;
   let svgoutline = `<path d="M18 12L6 12" stroke="${color}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> `;
   let svgsolid = `<path fill-rule="evenodd" clip-rule="evenodd" d="M5.25 12C5.25 11.5858 5.58579 11.25 6 11.25L18 11.25C18.4142 11.25 18.75 11.5858 18.75 12C18.75 12.4142 18.4142 12.75 18 12.75L6 12.75C5.58579 12.75 5.25 12.4142 5.25 12Z" fill="${color}"/> `;
   $: switch (variation) {
     case 'outline':
       svgpath = svgoutline;
-      viewBox = '0 0 24 24';
       break;
     case 'solid':
       svgpath = svgsolid;
-      viewBox = '0 0 20 20';
-      break;
     default:
       svgpath = svgoutline;
-      viewBox = '0 0 24 24';
   }
   export let ariaLabel = 'minus small';
 </script>
